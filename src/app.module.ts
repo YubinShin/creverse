95a214b433a1ca1e661b57e02c189bc94ceda2f1
Yubin Shin <shin.yubin18@gmail.com>
2025-08-25 15:03:45 +0900

    feat: register auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './modules/auth/auth.controller';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [AuthModule],
+  controllers: [AuthController],
   providers: [],
 })
 export class AppModule {}
