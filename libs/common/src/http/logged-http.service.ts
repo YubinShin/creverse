7fbe7ae61c72b82314232cc3e8b5c54435ed16f9
Yubin Shin <shin.yubin18@gmail.com>
2025-08-26 21:20:32 +0900

    feat: type 해결

--- libs/common/src/http/logged-http.service.ts
+++ libs/common/src/http/logged-http.service.ts
@@ -1,9 +1,7 @@
-// libs/common/http/logged-http.service.ts
-
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
-import { catchError, lastValueFrom, tap } from 'rxjs';
+import { catchError, lastValueFrom, map, tap } from 'rxjs';
 
 @Injectable()
 export class LoggedHttpService {
@@ -11,7 +9,7 @@ export class LoggedHttpService {
 
   constructor(private readonly http: HttpService) {}
 
-  async postWithLog<T = any>(
+  async postWithLog<T = unknown>(
     url: string,
     data: any,
     config: Record<string, any>,
@@ -19,9 +17,10 @@ export class LoggedHttpService {
     label = 'external-post',
   ): Promise<T> {
     const start = Date.now();
+
     return await lastValueFrom(
       this.http.post<T>(url, data, config).pipe(
-        tap((response: AxiosResponse) => {
+        tap((response) => {
           this.logger.log({
             traceId,
             label,
@@ -31,14 +30,16 @@ export class LoggedHttpService {
             latency: Date.now() - start,
           });
         }),
-        catchError((error) => {
+        map((response: AxiosResponse<T>) => response.data),
+        catchError((error: unknown) => {
+          const err = error as Error;
           this.logger.error({
             traceId,
             label,
             url,
             status: 'failed',
             latency: Date.now() - start,
-            message: error.message,
+            message: err?.message ?? 'unknown error',
           });
           throw error;
         }),
